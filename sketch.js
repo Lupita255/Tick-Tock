f912e804f44a2aa7efbb86cf78fb92187728100e
Lupita255 <75394038+Lupita255@users.noreply.github.com>
2021-03-11 17:37:21 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,9 +16,9 @@ function draw() {
   scAngle = map(sc,0,60,0,360);
   mnAngle = map(mn,0,60,0,360);
   hrAngle = map(hr % 12,0,12,0,360);
+
   push();
   rotate(scAngle);
-
   stroke(255,0,0);
   strokeWeight(7);
   line(0,0,100,0);
@@ -26,11 +26,18 @@ function draw() {
 
   push();
   rotate(hrAngle);
-
   stroke(0,0,255);
   strokeWeight(7);
   line(0,0,50,0);
   pop();
+
+  push();
+  rotate(mnAngle);
+  stroke(0,255,0);
+  strokeWeight(7);
+  line(0,0,75,0);
+  pop();
+
   stroke(255,0,255);
   point(0,0);
   strokeWeight(10);
@@ -43,4 +50,5 @@ function draw() {
   arc(0,0,260,260,0,hrAngle);
 
   drawSprites();
+  
 }
